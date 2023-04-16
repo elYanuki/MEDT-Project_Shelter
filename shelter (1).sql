973a1064cd686b1a8b9f1f2b53621026fd60082a
elYanuki <pc.mail.yanik@gmail.com>
2023-04-16 22:39:04 +0200

    sql export

--- shelter (1).sql
+++ shelter (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 14. Apr 2023 um 18:27
--- Server-Version: 10.4.25-MariaDB
--- PHP-Version: 8.1.10
+-- Erstellungszeit: 16. Apr 2023 um 22:38
+-- Server-Version: 10.4.27-MariaDB
+-- PHP-Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,7 @@ CREATE TABLE `animal` (
   `roomID` int(3) NOT NULL,
   `foodType` varchar(50) DEFAULT NULL,
   `foodTime` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `animal`
@@ -50,7 +50,7 @@ CREATE TABLE `animal` (
 
 INSERT INTO `animal` (`ID`, `shelter_email`, `name`, `note`, `typeID`, `breedID`, `image`, `gender`, `birthdate`, `acquiryDate`, `ownerID`, `roomID`, `foodType`, `foodTime`) VALUES
 (42, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 0, 1, NULL, NULL),
-(43, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 0, 1, NULL, NULL),
+(43, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 2, 3, NULL, NULL),
 (44, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 0, 1, NULL, NULL),
 (45, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 0, 1, NULL, NULL),
 (46, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-13', 0, 1, NULL, NULL),
@@ -66,7 +66,21 @@ INSERT INTO `animal` (`ID`, `shelter_email`, `name`, `note`, `typeID`, `breedID`
 (56, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-14', 0, 1, NULL, NULL),
 (57, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-14', 0, 1, NULL, NULL),
 (58, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-14', 0, 1, NULL, NULL),
-(59, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-14', 0, 1, NULL, NULL);
+(59, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-14', 0, 1, NULL, NULL),
+(60, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-15', 0, 1, NULL, NULL),
+(61, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-15', 0, 1, NULL, NULL),
+(62, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-15', 0, 1, NULL, NULL),
+(63, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-16', 0, 1, NULL, NULL),
+(64, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-16', 0, 1, NULL, NULL),
+(65, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-16', 0, 3, NULL, NULL),
+(66, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-16', 0, 3, NULL, NULL),
+(67, 'yanik@kendler.me', 'animal', NULL, 1, 0, NULL, 0, NULL, '2023-04-16', 0, 3, NULL, NULL),
+(68, 'yanik@kendler.me', 'animal', NULL, 1, 11, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL),
+(69, 'yanik@kendler.me', 'animal', NULL, 1, 11, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL),
+(70, 'yanik@kendler.me', 'animal', NULL, 1, 13, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL),
+(71, 'yanik@kendler.me', 'animal', NULL, 2, 14, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL),
+(72, 'yanik@kendler.me', 'animal', NULL, 3, 16, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL),
+(73, 'yanik@kendler.me', 'animal', NULL, 1, 13, NULL, 0, NULL, '2023-04-16', 0, 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -79,7 +93,7 @@ CREATE TABLE `animal_breed` (
   `shelter_email` varchar(100) NOT NULL,
   `animal_type_ID` int(4) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `animal_breed`
@@ -105,7 +119,7 @@ CREATE TABLE `animal_type` (
   `ID` int(4) NOT NULL,
   `shelter_email` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `animal_type`
@@ -130,14 +144,17 @@ CREATE TABLE `owner` (
   `tel` varchar(30) NOT NULL,
   `email` varchar(100) NOT NULL,
   `note` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `owner`
 --
 
 INSERT INTO `owner` (`ID`, `shelter_email`, `name`, `adress`, `tel`, `email`, `note`) VALUES
-(1, 'yanik@kendler.com', 'motty', 'iwo in talheim', '04353453', 'motty@msf.sadf', 'whats poppin');
+(1, 'yanik@kendler.com', 'motty', 'iwo in talheim', '04353453', 'motty@msf.sadf', 'whats poppin'),
+(2, 'yanik@kendler.me', 'felix', 'zuhause', '121345667', 'eine@email.adresse', ''),
+(3, 'yanik@kendler.me', 'leon', 'zuhause', '121345667', 'eine@email.adresse', ''),
+(4, 'yanik@kendler.me', 'hugo', 'zuhause', '121345667', 'eine@email.adresse', '');
 
 -- --------------------------------------------------------
 
@@ -150,14 +167,18 @@ CREATE TABLE `room` (
   `shelter_email` varchar(100) NOT NULL,
   `size` int(2) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `room`
 --
 
 INSERT INTO `room` (`ID`, `shelter_email`, `size`, `name`) VALUES
-(1, 'yanik@kendler.com', 23, 'cool');
+(1, 'yanik@kendler.com', 23, 'cool'),
+(2, 'yanik@kendler.me', 5, 'holzhütte'),
+(3, 'yanik@kendler.me', 5, 'hundehaus'),
+(4, 'yanik@kendler.me', 5, 'käfig 1'),
+(5, 'yanik@kendler.me', 2, 'käfig 2');
 
 -- --------------------------------------------------------
 
@@ -170,7 +191,7 @@ CREATE TABLE `user` (
   `name` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `admin_password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `user`
@@ -202,6 +223,18 @@ ALTER TABLE `animal_type`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indizes für die Tabelle `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indizes für die Tabelle `user`
 --
 ALTER TABLE `user`
@@ -215,7 +248,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `animal`
 --
 ALTER TABLE `animal`
-  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT für Tabelle `animal_breed`
@@ -228,6 +261,18 @@ ALTER TABLE `animal_breed`
 --
 ALTER TABLE `animal_type`
   MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT für Tabelle `room`
+--
+ALTER TABLE `room`
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
